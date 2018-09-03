ec121ba193f81b4071ce88e43c5ef04b0645fd63
Jayden Park <jaydenp@student.unimelb.edu.au>
2018-09-04 09:24:19 +1000

    updated test script

--- test/unit/components/StarTextItem.js
+++ test/unit/components/StarTextItem.js
@@ -4,10 +4,15 @@ import { expect } from 'chai';
 import StarTextItem from "../../../src/components/StarTextItem";
 
 describe('StarTextItem', () => {
-  it('renders correctly', () => {
-    const wrapper = shallow(
-      <StarTextItem rating={3} onRatingChange={() => null} width={1000} />
-    );
-    expect(wrapper).to.matchSnapshot();
-  });
+    it('renders correctly', () => {
+        const wrapper = shallow(
+          <StarTextItem
+              rating={3}
+              onRatingChange={() => null}
+              width={1000}
+          />
+        );
+
+        expect(wrapper).to.matchSnapshot();
+    });
 });
