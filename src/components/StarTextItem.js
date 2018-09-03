69a3d5f72ad9c886beffe9684c46b42e75aa024b
Jayden Park <jaydenp@student.unimelb.edu.au>
2018-09-04 09:24:03 +1000

    fixed startestitem

--- src/components/StarTextItem.js
+++ src/components/StarTextItem.js
@@ -2,42 +2,78 @@
 import React from "react";
 import Star from "./Stars";
 
+/**
+ * What to divide the star dimensions by depending on screen size.
+ */
+const starDimensionDivisor = {
+    small: 8,
+    large: 6,
+    max: 120,
+}
+
+/**
+ * What to divide the star spacing by depending on screen size.
+ */
+const starSpacingDivisor = {
+    small: 80,
+    large: 60,
+    max: 12,
+}
+
+/**
+ * Dictates the minimum viewport width required to show star text.
+ */
+const minimalWidthForStarText = 600;
+
+/**
+ * Dictates the maximum width of the stars.
+ */
+const maximumWidth = 1000;
+
 export default class StarTextItem extends React.Component {
 
-
-    minimalWidthForStarText = 600;
-    maximumWidth = 1000;
-
     props: {
+        /**
+         * The callback to call if the star value is changed.
+         */
         onRatingChange: Function,
+        /**
+         * The width of the viewport.
+         */
         width: number,
+        /**
+         * The pre-defined rating value for the stars.
+         */
         rating: number,
     };
 
+    /**
+     * @override
+     */
     render() {
-
         let windowsWidth = this.props.width;
-        let starDimension, starSpacing, onRatingChange;
+        let starDimension, starSpacing;
 
-
-        if (windowsWidth >= this.minimalWidthForStarText) {
-            starDimension = `${parseInt(windowsWidth / 8)}px`;
-            starSpacing = `${parseInt(windowsWidth / 80)}px`;
+        if (windowsWidth >= minimalWidthForStarText) {
+            starDimension = `${parseInt(
+                windowsWidth / starDimensionDivisor.small
+            )}px`;
+            starSpacing = `${parseInt(
+                windowsWidth / starSpacingDivisor.small
+            )}px`;
             // define the maximum star dimension
-            if (windowsWidth > this.maximumWidth) {
-                starDimension = "120px";
-                starSpacing = "12px";
+            if (windowsWidth > maximumWidth) {
+                starDimension = `${starDimensionDivisor.max}px`;
+                starSpacing = `${starSpacingDivisor.max}px`;
             }
         } else {
         // text will be hidden
-            starDimension = `${parseInt(windowsWidth / 6)}px`;
-            starSpacing = `${parseInt(windowsWidth / 60)}px`;
-        }
-
-        if (this.props.onRatingChange != undefined) {
-            onRatingChange = this.props.onRatingChange.bind(this);
-        } else {
-            onRatingChange = undefined;
+            starDimension = `${parseInt(
+                windowsWidth / starDimensionDivisor.large
+            )}px`;
+            starSpacing = `${parseInt(
+                windowsWidth / starDimensionDivisor.large
+            )}px`;
         }
 
         return (
@@ -48,7 +84,7 @@ export default class StarTextItem extends React.Component {
                         starDimension={starDimension}
                         starSpacing={starSpacing}
                         rating={this.props.rating || undefined}
-                        changeRating={onRatingChange}
+                        changeRating={this.props.onRatingChange}
                     />
                 </div>
                 {this.renderRightStarText()}
@@ -56,10 +92,15 @@ export default class StarTextItem extends React.Component {
         );
     }
 
+    /**
+     * Renders the text to the left of the star.
+     *
+     * @returns {ReactDOM.Element} The rendered text element.
+     */
     renderLeftStarText() {
         let windowsWidth = this.props.width;
 
-        if (windowsWidth < this.minimalWidthForStarText) {
+        if (windowsWidth < minimalWidthForStarText) {
             return null
         } else {
             return (
@@ -70,10 +111,15 @@ export default class StarTextItem extends React.Component {
         }
     }
 
+    /**
+     * Renders the text to the right of the star.
+     *
+     * @returns {ReactDOM.Element} The rendered text element.
+     */
     renderRightStarText() {
         let windowsWidth = this.props.width;
 
-        if (windowsWidth < this.minimalWidthForStarText) {
+        if (windowsWidth < minimalWidthForStarText) {
             return null
         } else {
             return (
---
+++ test/unit/components/StarTextItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import StarTextItem from "../../../src/components/StarTextItem";
+
+describe('StarTextItem', () => {
+  it('renders correctly', () => {
+    const wrapper = shallow(
+      <StarTextItem rating={3} onRatingChange={() => null} width={1000} />
+    );
+    expect(wrapper).to.matchSnapshot();
+  });
+});
